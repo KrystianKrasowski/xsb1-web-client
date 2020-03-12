3708da4b1c980512086104553ae472378c40961a
krystian.krasowski <krystiankrasowski@gmail.com>
2020-03-12 19:07:53 +0100

    added sets component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
+import { SetsComponent } from './sets/sets.component';
+import {RouterModule} from "@angular/router";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    RouterModule.forRoot([
+      { path: '', component: DashboardComponent },
+      { path: 'sets', component: SetsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
